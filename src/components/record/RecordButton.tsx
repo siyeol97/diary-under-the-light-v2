c935506c9ea2d7c21eb1c4f69f5069ea91895114
siyeol97 <leesi2830@naver.com>
2025-03-04 20:45:37 +0900

    ✨ feat: STTResult 컴포넌트 추가 및 녹음 결과 렌더링 통합

--- src/components/record/RecordButton.tsx
+++ src/components/record/RecordButton.tsx
@@ -3,6 +3,7 @@
 import { Button } from '@/components/ui/button';
 import useRecord from '@/hooks/useRecord';
 import { Session } from 'next-auth';
+import STTResult from '../main/STTResult';
 
 interface Props {
   session: Session;
@@ -37,16 +38,12 @@ export default function RecordButton({ session, date, transcode }: Props) {
       ) : processingText ? (
         <p>{processingText}</p>
       ) : audioURL ? (
-        // 녹음 완료 후, stt 결과와 음성 렌더링
-        <div className='flex flex-col items-center gap-2'>
-          <audio controls src={audioURL}></audio>
-          <textarea
-            className='border border-black min-w-full min-h-[160px]'
-            value={sttText}
-            onChange={(e) => updateSttText(e)}
-          />
-          <Button onClick={() => analyze()}>분석 시작</Button>
-        </div>
+        <STTResult
+          audioURL={audioURL}
+          sttText={sttText}
+          updateSttText={updateSttText}
+          analyze={analyze}
+        />
       ) : (
         <Button onClick={startRecording}>녹음 시작</Button>
       )}
